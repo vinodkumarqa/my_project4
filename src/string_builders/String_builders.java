3d43bc87212f646603f2c1a496546ac75154579e
Admin <Admin@DESKTOP-1L1HD6G>
2024-01-02 14:38:53 +0530

    initial Commit

---
+++ src/string_builders/String_builders.java
@@ -0,0 +1,18 @@
+package string_builders;
+
+public class String_builders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder st=new StringBuilder("welcome");
+		//st.append("marolix");
+				//st.reverse();
+				//st.replace(0, 1, "m");
+				//st.delete(0, 5);
+				//st.deleteCharAt(2);
+				st.insert(0, "m");
+				System.out.println(st);
+
+	}
+
+}
